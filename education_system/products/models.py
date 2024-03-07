6448ee347ce7d3e268d4eef9e47ed1cf311d2c97
matveyvarlamov <matvejvarlamov495@gmail.com>
2024-03-07 19:53:32 +0700

    update models files

--- education_system/products/models.py
+++ education_system/products/models.py
@@ -16,14 +16,14 @@ class Product(models.Model):
 
 class ProductValid(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
 
     class Meta:
         unique_together = ('user', 'product')
 
 
 class Lesson(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
     name_lesson = models.CharField(max_length=256)
     url_lesson = models.URLField()
 
@@ -33,13 +33,13 @@ class Lesson(models.Model):
 
 class Group(models.Model):
     name_group = models.CharField(max_length=256)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='groups')
-    students = models.ManyToManyField(User, related_name='groups', through='Enrollment')
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='groups')
+    students = models.ManyToManyField(User, related_name='student_groups', through='Enrollment')
 
     def __str__(self):
         return self.name_group
 
 
 class Enrollment(models.Model):
-    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    group = models.ForeignKey('Group', on_delete=models.CASCADE)
     student = models.ForeignKey(User, on_delete=models.CASCADE)
--- education_system/users/models.py
+++ education_system/users/models.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.models import AbstractUser
-
-
-class Author(AbstractUser):
-    pass
+from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+
+class Author(User):
+    is_author = models.BooleanField(default=False)
